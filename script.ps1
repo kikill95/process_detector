9bb5e29cb5335d3ceea5c93bfd8e220b770a8094
Kirill Gusyatin <kirill.gus@gmail.com>
2017-08-23 18:47:46 +0500

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -6,7 +6,11 @@ $WshShell = new-object -comobject "WScript.Shell" # Instantiate the wscript.shel
 
 if ($path -ne "") {
     $shortcuts = get-childitem -path $path -filter "*.lnk" -rec # Find all .lnk files, recursing in to subdirectories
-    ('[' + ($shortcuts | foreach-object { ($WshShell.CreateShortcut($_.FullName) | ConvertTo-Json) + ',' }) | %{$_.Substring(0, $_.length - 1) }) + ']'
+    ('[' +
+      ($shortcuts | foreach-object {
+        ($WshShell.CreateShortcut($_.FullName) | ConvertTo-Json) + ','
+      }) | %{$_.Substring(0, $_.length - 1) }
+    ) + ']'
     # For each file, pass the fullname to the COM object to open the shortcut and enumerate the properties
 } else {
     write-output "No arguments specified, please use either -p to specify a path"
