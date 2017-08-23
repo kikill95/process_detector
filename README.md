1e34b311f4b7ac3db1bdf664609ddacf0687f775
Light <kirill.gusyatin@ekreative.com>
2017-08-23 12:52:58 +0500

    Add readme.md for info how to run scripts, edit powershell's script

---
+++ README.md
@@ -0,0 +1,15 @@
+Bourne shell
+
+    ./script.sh
+
+Powershell
+
+     powershell -executionpolicy bypass -File ./script.ps1 -p /c/ProgramData/Microsoft/Windows/Start\ Menu/Programs/
+
+Powershell to JSON file
+
+    powershell -executionpolicy bypass -File ./script.ps1 -p /c/ProgramData/Microsoft/Windows/Start\ Menu/Programs/ > installed_apps.json
+
+Start menu
+
+    cd /c/ProgramData/Microsoft/Windows/Start\ Menu/Programs
--- script.ps1
+++ script.ps1
@@ -1,101 +1,13 @@
-function Get-InstalledAppsFromRegistry
-{
+param(
+   [string] $path = ""
+   )
 
-    $scriptBlock={
+$WshShell = new-object -comobject "WScript.Shell" # Instantiate the wscript.shell COM object
 
-        #this function gets all properties from registry
-        #It doesn't fail if a property value is corrupted
-
-        function read-AppPropertiesToObj{
-            param($Application, $Architecture)
-
-            $prop = @{
-                APP_Architecture = $Architecture
-                APP_GUID = $Application.PSChildName
-            }
-
-            #for PS 2.0 compatibility
-            $itemslist = @($Application | select -ExpandProperty Property)
-
-            foreach ($item in $itemslist)
-            {
-                #if value is corrupted, get-itemproperty function fails
-                try
-                {
-                    $prop.$item = $Application | Get-ItemProperty -name $item | select -ExpandProperty $item
-                }
-                catch
-                {
-                    $prop.$item = '(invalid value)'
-                }
-            }
-
-            $result = New-Object psobject -Property $prop
-
-            return $result
-        }#function
-
-        $apps = @()
-        $results = @()
-
-        if (Test-Path 'HKLM:\SOFTWARE\Wow6432Node\microsoft\Windows\CurrentVersion\Uninstall')
-        {
-            #
-            #"64 bit system, 32 bit node"
-            $apps = Get-ChildItem 'HKLM:\SOFTWARE\Wow6432Node\microsoft\Windows\CurrentVersion\Uninstall'
-
-            foreach ($app in $apps)
-            {
-                $results += read-AppPropertiesToObj -Application $app -Architecture 32
-            }
-
-            #64 bit system, 64 bit node
-            $apps = Get-ChildItem 'HKLM:\SOFTWARE\microsoft\Windows\CurrentVersion\Uninstall'
-
-            foreach ($app in $apps)
-            {
-                $results += read-AppPropertiesToObj -Application $app -Architecture 64
-            }
-
-        }
-        else
-        {
-            #32 bit system, 32 bit node
-            $apps = Get-ChildItem 'HKLM:\SOFTWARE\microsoft\Windows\CurrentVersion\Uninstall'
-
-            foreach ($app in $apps)
-            {
-                $results += read-AppPropertiesToObj -Application $app -Architecture 32
-            }
-        }#if else
-
-        return $results | sort DisplayName
-
-    }#scriptblock
-
-    #determine OS architecture and path to the native powershell.exe
-
-    #-----32 bit process running on a 64 bit machine
-    if (([intptr]::size -eq 4) -and (test-path env:\PROCESSOR_ARCHITEW6432))
-    {
-        $PsExePath = "C:\windows\sysnative\WindowsPowerShell\v1.0\powershell.exe"
-    }
-    #-----64 bit process running on a 64 bit machine
-    elseif (([intptr]::size -eq 8) -and !(test-path env:\PROCESSOR_ARCHITEW6432))
-    {
-        $PsExePath = "C:\windows\system32\WindowsPowerShell\v1.0\powershell.exe"
-    }
-    #-----32 bit process running on a 32 bit machine
-    else
-    {
-        $PsExePath = "C:\windows\system32\WindowsPowerShell\v1.0\powershell.exe"
-    }
-
-    #execute command
-    $tmp = & $PsExePath $scriptBlock
-
-    #return results
-    return $tmp
+if ($path -ne "") {
+    $shortcuts = get-childitem -path $path -filter "*.lnk" -rec # Find all .lnk files, recursing in to subdirectories
+    ('[' + ($shortcuts | foreach-object { ($WshShell.CreateShortcut($_.FullName) | ConvertTo-Json) + ',' }) | %{$_.Substring(0, $_.length - 1) }) + ']'
+    # For each file, pass the fullname to the COM object to open the shortcut and enumerate the properties
+} else {
+    write-output "No arguments specified, please use either -p to specify a path or -f for a specific .lnk file"
 }
-
-Get-InstalledAppsFromRegistry
